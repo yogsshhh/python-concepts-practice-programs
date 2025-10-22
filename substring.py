142bfe8d9ec3dedb6aa03cf5214dfb5e8a4f4bd9
yogsshhh <165871464+yogsshhh@users.noreply.github.com>
2025-10-22 14:08:31 +0530

    Update substring.py

--- substring.py
+++ substring.py
@@ -1,11 +1,11 @@
-🧩 3️⃣ Subarray with Specific (Fixed) Size
+# 🧩 3️⃣ Subarray with Specific (Fixed) Size
 
-Model: Find a subarray of size K satisfying a condition.
+# Model: Find a subarray of size K satisfying a condition.
 
-Q: Find the maximum sum subarray of size 3.
-Input: arr = [2, 1, 5, 1, 3, 2], k = 3
-Output: 9
-Explanation: Subarray [5, 1, 3] → sum = 9 (maximum among all subarray).
+# Q: Find the maximum sum subarray of size 3.
+# Input: arr = [2, 1, 5, 1, 3, 2], k = 3
+# Output: 9
+# Explanation: Subarray [5, 1, 3] → sum = 9 (maximum among all subarray).
 
 arr=[2, 1, 5, 1, 3, 2]
 k=3
@@ -19,3 +19,52 @@ for i in range(len(arr)):
         if temp > big:
             big=temp
 print(big)
+----------------------------------
+arr=[2, 1, 5, 1, 3, 2]
+k=3
+big=0
+for i in range(len(arr)-k+1):
+        sub=sum(arr[i:k+1])
+        if sub > big:
+            big=sub
+print(big)
+
+# 🧩 5️⃣ Subarray — All Pairs (2-element subarray)
+
+# Model: Work with pairs of elements (subarrays of size 2).
+
+# Q: Print all pairs and their sums.
+# Input: arr = [4, 7, 1]
+# Output:
+
+# Pairs: (4,7), (4,1), (7,1)
+# Sums: 11, 5, 8
+
+from itertools import combinations
+arr = [4, 7, 1]
+comb=list(combinations(arr,2))
+sums=[]
+for i in comb:
+    k=sum(i)
+    sums.append(k)
+print(*comb)
+print(*sums)
+
+# Model: Work with consecutive characters in a string.
+
+# Q: Print all substrings of "abc".
+# Input: "abc"
+# Output:
+
+# a
+# ab
+# abc
+# b
+# bc
+# c
+    
+n="abcd"
+for i in range(len(n)):
+    for j in range(i,len(n)):
+        sub=n[i:j+1]
+        print(sub)
