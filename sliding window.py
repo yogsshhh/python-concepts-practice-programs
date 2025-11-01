6a1a50323a657eacb708f248dd0c58740309a5ca
yogsshhh <165871464+yogsshhh@users.noreply.github.com>
2025-11-01 15:04:25 +0530

    Update sliding window.py

--- sliding window.py
+++ sliding window.py
@@ -1,1 +1,19 @@
+-----SLIDING WINDOW
+-------------SHRINK WINDOW/RESET POINTER BY RULES
 
+1.lenght of longest substring
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = set()
+        left = 0
+        maxi = 0
+
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+            seen.add(s[right])
+            maxi = max(maxi, right - left + 1)
+
+        return maxi
