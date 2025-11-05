0d6e2b3e71e26451f31c7195f3a2ff4f99c90c29
yogsshhh <165871464+yogsshhh@users.noreply.github.com>
2025-11-05 22:55:12 +0530

    Update Accenture previous.py

--- Accenture previous.py
+++ Accenture previous.py
@@ -301,11 +301,22 @@ for i in range(1, n + 1):
 print(c)
 
 
+    1
+   212
+  32123
+ 4321234
+543212345
 
 
-
-
-
+n=5
+for i in range (1,n+1):
+    for j in range(n-i):
+        print(" ",end="")
+    for k in range(i,0,-1):
+        print(k,end="")
+    for j in range(2,i+1):
+        print(j,end="")
+    print()
 
 
 
