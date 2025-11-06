2374ed5423f1912aaa23ed8d79aa66ede1788988
yogsshhh <165871464+yogsshhh@users.noreply.github.com>
2025-11-06 20:37:48 +0530

    Update imp_algo.py

--- imp_algo.py
+++ imp_algo.py
@@ -1,1 +1,10 @@
+# kadne's algorithm maximum sum of subarray
+arr = [-2,1,-3,4,-1,2,1,-5,4]
 
+max_ending = max_so_far = arr[0]
+
+for x in arr[1:]:
+    max_ending = max(x, max_ending + x)
+    max_so_far = max(max_so_far, max_ending)
+
+print(max_so_far)
