fe554707fcb6dece8f0c36ac093b5dbd795be390
yogsshhh <165871464+yogsshhh@users.noreply.github.com>
2025-11-07 17:46:36 +0530

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -320,12 +320,28 @@ for i in range(n):
     print()
 
 
+     1 
+    1 2 1 
+   1 2 3 2 1 
+  1 2 3 4 3 2 1 
+ 1 2 3 4 5 4 3 2 1 
 
 
 
 
-
-
+n=5
+p=1
+for i in range(n):
+    p=1
+    for j in range(i,n):
+        print(" ",end="")
+    for j in range(i):
+        print(p,end=" ")
+        p+=1
+    for j in range(i+1):
+        print(p,end=" ")
+        p-=1
+    print()
 
 
 
